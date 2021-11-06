ececf00f6baf601758224df37812e30a455f03ab
mahdi fazli <mahdifazli5459@gmail.com>
2021-11-06 15:17:07 +0330

    Fixed some miner bugs

--- script.js
+++ script.js
@@ -28,9 +28,9 @@ function animatedForm() {
 
 function validateUser(user) {
     if (user.value.length < 6) {
-        error('rgb(189,87,87)', 'rgb(87,189,130)','User name should have 6 character');
+        error('rgb(189,87,87)', 'User name should have 6 character');
     } else {
-        error('rgb(87,189,130)', '#000','Please enter your email address');
+        error('rgb(87,189,130)', 'Please enter your email address');
         return true;
     }
 }
@@ -38,18 +38,18 @@ function validateUser(user) {
 function validateEmail(email) {
     const validation = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (validation.test(email.value)) {
-        error('rgb(87,189,130)', '#000','Please enter your password');
+        error('rgb(87,189,130)', 'Please enter your password');
         return true;
     } else {
-        error('rgb(189,87,87)', '#70DBDB', 'This is not a valid email address');
+        error('rgb(189,87,87)',  'This is not a valid email address');
     }
 }
 
 function validatePassword(password) {
     if (password.value.length < 8) {
-        error('rgb(189,87,87)', '#70DBDB','Password should have 8 character');
+        error('rgb(189,87,87)', 'Password should have 8 character');
     } else {
-        error('rgb(87,189,130)', '#000','we are done!');
+        error('rgb(87,189,130)', 'we are done!');
         return true;
     }
 }
@@ -60,15 +60,14 @@ function nextSlide(parent, nextForm) {
     nextForm.classList.add("active");
 }
 
-function error(backgroundColor, textColor, errorMessage) {
+function error(backgroundColor, errorMessage) {
     document.body.style.backgroundColor = backgroundColor;
-    document.querySelector(".errorMessage").style.color = textColor;
     document.querySelector('.message').textContent = errorMessage;
-    if (textColor === '#70DBDB') {
+    if (backgroundColor === 'rgb(189,87,87)') {
         document.querySelector('i.far').classList.remove('fa-smile');
         document.querySelector('i.far').classList.add('fa-frown');
     }
-    if (textColor === '#000') {
+    if (backgroundColor === 'rgb(87,189,130)') {
         document.querySelector('i.far').classList.remove('fa-frown');
         document.querySelector('i.far').classList.add('fa-smile');
     }
